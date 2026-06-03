663ecf1e395bee763d151635ce684b143909041c
Prathmesh Nakate <prathmeshnakate24@gmail.com>
2026-06-03 15:50:54 +0530

    updated main.py From The basic Code To The Advanced Code

--- main.py
+++ main.py
@@ -1,48 +1,104 @@
 import customtkinter as ctk
 
-# Appearance
+from ai_helper import generate_summary
+from resume_generator import create_resume
+from database import save_resume
+from database import create_database
+
+create_database()
+
 ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("blue")
 
-# Main Window
 app = ctk.CTk()
+app.geometry("900x700")
 app.title("AI Resume Builder")
-app.geometry("900x600")
 
-# Title
-title_label = ctk.CTkLabel(
+title = ctk.CTkLabel(
     app,
     text="AI Resume Builder",
-    font=("Arial", 28, "bold")
+    font=("Arial", 30, "bold")
 )
-title_label.pack(pady=20)
 
-# Name
-name_label = ctk.CTkLabel(app, text="Full Name")
-name_label.pack()
+title.pack(pady=15)
 
-name_entry = ctk.CTkEntry(app, width=400)
-name_entry.pack(pady=5)
+entries = {}
 
-# Email
-email_label = ctk.CTkLabel(app, text="Email")
-email_label.pack()
+fields = [
+    "Name",
+    "Email",
+    "Role",
+    "Education",
+    "Skills",
+    "Projects"
+]
 
-email_entry = ctk.CTkEntry(app, width=400)
-email_entry.pack(pady=5)
+for field in fields:
 
-# Skills
-skills_label = ctk.CTkLabel(app, text="Skills")
-skills_label.pack()
+    label = ctk.CTkLabel(app,
+                         text=field)
 
-skills_entry = ctk.CTkEntry(app, width=400)
-skills_entry.pack(pady=5)
+    label.pack()
 
-# Generate Button
-generate_btn = ctk.CTkButton(
+    entry = ctk.CTkEntry(
+        app,
+        width=500
+    )
+
+    entry.pack(pady=5)
+
+    entries[field] = entry
+
+
+def build_resume():
+
+    name = entries["Name"].get()
+    email = entries["Email"].get()
+    role = entries["Role"].get()
+    education = entries["Education"].get()
+    skills = entries["Skills"].get()
+    projects = entries["Projects"].get()
+
+    summary = generate_summary(
+        role,
+        skills
+    )
+
+    create_resume(
+        f"resumes/{name}.pdf",
+        name,
+        email,
+        education,
+        skills,
+        projects,
+        summary
+    )
+
+    save_resume(
+        name,
+        email,
+        education,
+        skills,
+        projects
+    )
+
+    result.configure(
+        text="Resume Generated Successfully!"
+    )
+
+
+btn = ctk.CTkButton(
     app,
-    text="Generate Resume"
+    text="Generate Resume",
+    command=build_resume
 )
-generate_btn.pack(pady=20)
+
+btn.pack(pady=20)
+
+result = ctk.CTkLabel(
+    app,
+    text=""
+)
+
+result.pack()
 
 app.mainloop()
