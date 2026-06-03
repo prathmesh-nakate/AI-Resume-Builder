9061472ffc21f63dca11e05a8959640494a0fee0
Prathmesh Nakate <prathmeshnakate24@gmail.com>
2026-06-03 16:02:06 +0530

    Updated ai helper

--- ai_helper.py
+++ ai_helper.py
@@ -9,3 +9,17 @@ API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=API_KEY)
 
 model = genai.GenerativeModel("gemini-1.5-flash")
+
+def generate_summary(role, skills):
+    prompt = f"""
+    Write a professional resume summary.
+
+    Role: {role}
+    Skills: {skills}
+
+    Keep it under 80 words.
+    """
+
+    response = model.generate_content(prompt)
+
+    return response.text
