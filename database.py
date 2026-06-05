6dca2ad78764d39813641a1a782e6c41e166348e
Prathmesh Nakate <prathmeshnakate24@gmail.com>
2026-06-05 16:27:24 +0530

    Updated Database

--- database.py
+++ database.py
@@ -5,28 +5,53 @@ def create_database():
     cursor = conn.cursor()
 
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS resumes(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        email TEXT,
-        education TEXT,
-        skills TEXT,
-        projects TEXT
-    )
-    """)
-
+CREATE TABLE IF NOT EXISTS resumes(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT,
+    github TEXT,
+    linkedin TEXT,
+    education TEXT,
+    skills TEXT,
+    projects TEXT
+)
+""")
     conn.commit()
     conn.close()
 
 
-def save_resume(name, email, education, skills, projects):
+def save_resume(
+    name,
+    email,
+    github,
+    linkedin,
+    education,
+    skills,
+    projects
+):
     conn = sqlite3.connect("resume.db")
     cursor = conn.cursor()
 
     cursor.execute("""
-    INSERT INTO resumes(name,email,education,skills,projects)
-    VALUES(?,?,?,?,?)
-    """, (name, email, education, skills, projects))
+    INSERT INTO resumes(
+        name,
+        email,
+        github,
+        linkedin,
+        education,
+        skills,
+        projects
+    )
+    VALUES(?,?,?,?,?,?,?)
+    """, (
+        name,
+        email,
+        github,
+        linkedin,
+        education,
+        skills,
+        projects
+    ))
 
     conn.commit()
     conn.close()
